6e5197d93bfe571e95b514b44fc0524afb5af405
Kristoffer Pantic <kpantic@talpor.com>
2015-01-25 23:18:28 +0100

    Fixes for Django 1.7 support

--- blogango/apps.py
+++ blogango/apps.py
@@ -8,3 +8,12 @@ class BlogangoConfig(AppConfig):
     def ready(self):
         from south.modelsinspector import add_introspection_rules
         add_introspection_rules([], ["^markupfield\.fields\.MarkupField"])
+
+        # ping_details = {'blogango_details': pingback_blog_handler}
+        from pingback import register_pingback, ping_func
+        from . import pingback_blog_handler
+        from django_xmlrpc import xmlrpcdispatcher
+
+        register_pingback('blogango.views.details', pingback_blog_handler)
+
+        xmlrpcdispatcher.register_function(ping_func, 'pingback.ping')
